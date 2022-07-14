9574ee06e366d9bc86839557c1c7d94140fe39d6
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-07-14 10:36:12 +0700

    first commit

---
+++ automaticwa.py
@@ -0,0 +1,9 @@
+import pywhatkit
+import pyautogui
+import time
+
+pywhatkit.sendwhatmsg("+62xxxxxxxxx", "Halo nama saya Abid Juliant Indraswara", 9, 33)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.press('enter')
