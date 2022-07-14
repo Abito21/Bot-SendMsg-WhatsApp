794ce9cea5f356a68ababd21523994aad5b59d17
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-07-14 10:38:02 +0700

    Update Automatic WAMsg

--- automaticwa.py
+++ automaticwa.py
@@ -1,9 +1,13 @@
+import time
 import pywhatkit
 import pyautogui
-import time
 
+# Send Message
+# Hp Number, Message, time_hour, time_minute
 pywhatkit.sendwhatmsg("+62xxxxxxxxx", "Halo nama saya Abid Juliant Indraswara", 9, 33)
 time.sleep(1)
+# Click Message
 pyautogui.click()
 time.sleep(1)
+# Enter Message
 pyautogui.press('enter')
